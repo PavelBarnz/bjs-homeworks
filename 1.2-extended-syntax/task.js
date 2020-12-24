4b861f28f41b652e93f5851f411cfe8295a28623
Pavel Burchinskiy <p.barnz91@gmail.com>
2020-12-24 17:07:44 +0500

    Доделал Д\З по теме 1.2 Расшириный синтаксис

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -6,13 +6,10 @@ function getResult(a,b,c){
     let D = Math.pow(b, 2) - 4 * a * c;
 
     if(D > 0){
-       let x1 = (-b - Math.sqrt(D)) / (2 * a);
-       result.push(x1);
-       let x2 = (-b + Math.sqrt(D)) / (2 * a);
-       result.push(x2);
+        result.push((-b - Math.sqrt(D)) / (2 * a));
+        result.push((-b + Math.sqrt(D)) / (2 * a));
     } else if(D === 0) {
-        let x = (2 * a) / -b;
-        result.push(x);
+        result.push(-b / (2 * a));
     };
 
      return result;
@@ -23,33 +20,21 @@ function getAverageMark(marks){
 
     if(marks.length == 0) {
         return 0;
-    }else if(marks.length == 5){
-        for(let i = 0; i < marks.length; i++) {
-            averageMark += marks[i];
-        };
-        averageMark = averageMark / marks.length;
     }else if(marks.length > 5) {
-        console.log("Оценок больше 5");
-        marks.splice(5);
-        let i = 0;
-        while(true){
-          averageMark += marks[i];
-          i++;
-          if(i == 5) {break};
-        };
-        averageMark = averageMark / marks.length;
+      marks.splice(5);
     };
-    return averageMark;
+    for (let i = 0; i < marks.length; i++) {
+      averageMark += marks[i];
+    };
+        
+    return averageMark / marks.length;
 }
 
 function askDrink(name,dateOfBirthday){
     let age = new Date().getFullYear() - dateOfBirthday.getFullYear();
-    let result;
     if (age < 18) {
-       result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
+       return `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
     }else {
-        result = `Не желаете ли олд-фэшн, ${name}?`
+        return `Не желаете ли олд-фэшн, ${name}?`
     }
-    console.log(result);
-     return result;
 }
