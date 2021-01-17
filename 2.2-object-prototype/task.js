46dbaa6febfd3fada3817dad9feadbdd03026c8c
Pavel Burchinskiy <p.barnz91@gmail.com>
2021-01-17 18:55:36 +0500

    Выполнил задание по теме Обьекты Прототипы

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,31 @@
 //String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function (){
+    let arr = this.toLowerCase().split(" ");
+  
+    let str = arr.reduce(function(sum, item, i){
+      return sum += item;
+    });
+  
+    let arr2 = str.split("");
+  
+    let str2 = arr2.reverse().join("");
+  
+    return str == str2 ? true : false;
+  };
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if(marks.length === 0) {
+        return 0;
+    };
+
+    let averge = marks.reduce(function (sum, item, i){
+        return sum + item;
+    }, 0)
+
+    return Math.round(averge = averge / marks.length)
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
-}
+    
+};
